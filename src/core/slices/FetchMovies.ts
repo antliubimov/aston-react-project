2991206f8fe4e841e83ef0cce29636ef73bdd747
Natalya Koshukova <koshukovanat@gmail.com>
2024-03-25 14:43:22 +0700

    chore: mount: Navbar, Loader, SliderCarousel. Update: App, MainPage.

--- src/core/slices/FetchMovies.ts
+++ src/core/slices/FetchMovies.ts
@@ -4,12 +4,18 @@ import { movieSlice } from './MovieSlice';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
+// const API_key = `apikey=${process.env.REACT_APP_API_KEY}`;
+// const API_URL = `https://www.omdbapi.com/?${API_key}`
+// в работе
+
 export const fetchMovies = () => async (dispatch: Dispatch) => {
   try {
     dispatch(movieSlice.actions.moviesFetching());
     const response = await axios.get<FetchMoviesResponse>(
-      'https://www.omdbapi.com/?apikey=786d4402&s=day',
+      'https://www.omdbapi.com/?s=day&apikey=786d4402',
+      // `${API_URL}&s=day`,
     );
+
     dispatch(movieSlice.actions.moviesFetchingSuccess(response.data.Search));
   } catch (e: any) {
     dispatch(movieSlice.actions.moviesFetchingError(e.message));
